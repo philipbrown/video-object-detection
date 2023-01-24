f2b41752ff85de3345ac6f3034bbd5ef2b862db1
philipbrown <phil@ipbrown.com>
2023-01-24 12:35:02 +0000

    Set up new Phoenix project

---
+++ config/test.exs
@@ -0,0 +1,21 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :app, AppWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "q0tYGt8Eb+ythp6nNJahxI78e5izwlRje2wa1vMww4fXpRxUgZxHbXjm/088lvkm",
+  server: false
+
+# In test we don't send emails.
+config :app, App.Mailer,
+  adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters.
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
